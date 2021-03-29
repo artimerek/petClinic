f2adc3968e10088f80c009b66c0966e642f87935
Konradek <konradbiznesu@gmail.com>
2021-03-29 21:12:26 +0200

    Refactoring to multimodule maven project. closes #6.

--- pet-clinic-data/src/main/java/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/petclinic/model/Vet.java
@@ -1,4 +1,4 @@
-package pl.artimerek.petclinic.model;
+package petclinic.model;
 
 // closing
 
--- pet-clinic-web/src/main/java/petclinic/PetclinicApplication.java
+++ pet-clinic-web/src/main/java/petclinic/PetclinicApplication.java
@@ -1,4 +1,4 @@
-package pl.artimerek.petclinic;
+package petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

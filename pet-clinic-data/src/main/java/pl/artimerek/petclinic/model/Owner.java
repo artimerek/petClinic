32453f8ed3f69be35d31e5fa9b2c226c34a737e6
artimerek <konradbiznesu@gmail.com>
2021-05-02 21:40:49 +0200

    Visit entity

--- pet-clinic-data/src/main/java/pl/artimerek/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/artimerek/petclinic/model/Owner.java
@@ -2,5 +2,19 @@ package pl.artimerek.petclinic.model;
 
 // closing
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
 }
